26d460260e33b542a1f1dbbe4479e452d7b15feb
Kang An <kaan@c4b7c2.42berlin.de>
2024-07-04 16:59:05 +0200

    WIP

--- CPP00/ex01/contact.hpp
+++ CPP00/ex01/contact.hpp
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ex01.hpp                                           :+:      :+:    :+:   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaan <kaan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/03 18:31:38 by kaan              #+#    #+#             */
-/*   Updated: 2024/07/03 18:53:06 by kaan             ###   ########.fr       */
+/*   Created: 2024/07/04 14:14:15 by kaan              #+#    #+#             */
+/*   Updated: 2024/07/04 15:57:56 by kaan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
 #include <iostream>
 
-class phonebook
-{
-    
-};
-
 class contact
-{
+{   
+    private:
+    std::string _firstname;
+    std::string _lastname;
+    std::string _phone_number;
+    std::string _pet_name;
+    int         index;
+
+    public:
+    std:: string getFirstName();
+    void setFirstName();
     
 };
